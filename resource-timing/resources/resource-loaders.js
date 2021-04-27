c92567a0d924847f988f26ec53635159ea637e4b
Tom McKee <tommckee@chromium.org>
2021-04-27 06:33:10 -0700

    [ResourceTiming] Add cache busting to ResourceTiming WPTs
    In order to reliably force loading a given resource, the helper
    functions in resource-loaders.js now add a "cache_bust" search
    parameter to URLs before loading them. This way, cache state
    won't leak into and interfere with running multiple ResourceTiming
    tests.
    
    Bug: 1171767
    Change-Id: I9927963b0c467122c478cb6b137b7113487219d9
    GithubIssue: https://github.com/w3c/resource-timing/issues/254

--- resource-timing/resources/resource-loaders.js
+++ resource-timing/resources/resource-loaders.js
@@ -1,11 +1,21 @@
 const load = {
+  _cache_bust_value: Math.random().toString().substr(2),
+  cache_bust: path => {
+    let url = new URL(path, location.origin);
+    if (url.searchParams.has("cache_bust")) {
+      throw new Error(`${path} already has 'cache_bust' as a search parameter`);
+    }
+    url.searchParams.append("cache_bust", load._cache_bust_value++);
+    return url.href;
+  },
+
   // Returns a promise that settles once the given path has been fetched as an
   // image resource.
   image: path => {
     return new Promise(resolve => {
       const img = new Image();
       img.onload = img.onerror = resolve;
-      img.src = path;
+      img.src = load.cache_bust(path);
     });
   },
 
@@ -16,8 +26,8 @@ const load = {
     div.innerHTML = `
       <style>
       @font-face {
-          font-family: ahem;
-          src: url('${path}');
+          font-family: "ahem";
+          src: url('${load.cache_bust(path)}');
       }
       </style>
       <div style="font-family: ahem;">This fetches ahem font.</div>
@@ -34,7 +44,7 @@ const load = {
     const link = document.createElement("link");
     link.rel = "stylesheet";
     link.type = "text/css";
-    link.href = path;
+    link.href = load.cache_bust(path);
 
     const loaded = new Promise(resolve => {
       link.onload = link.onerror = resolve;
@@ -52,7 +62,7 @@ const load = {
     const loaded = new Promise(resolve => {
       frame.onload = frame.onerror = resolve;
     });
-    frame.src = path;
+    frame.src = load.cache_bust(path);
     document.body.appendChild(frame);
     await loaded;
     document.body.removeChild(frame);
@@ -65,7 +75,7 @@ const load = {
     const loaded = new Promise(resolve => {
       script.onload = script.onerror = resolve;
     });
-    script.src = path;
+    script.src = load.cache_bust(path);
     document.body.appendChild(script);
     await loaded;
     document.body.removeChild(script);
